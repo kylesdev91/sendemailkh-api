4bacfcab9bac1fda33abca1355dd1f2402a43ef3
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-23 21:53:50 -0500

    still works..

--- sendmail/index.js
+++ sendmail/index.js
@@ -1,27 +1,42 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 module.exports = async function (context, req) {
-    let transporter = nodemailer.createTransport({
-        service: "hotmail",
-        auth: {
-            user: "kffsande@outlook.com",
-            pass: "Pwd4Kff5and3"
-        },
-        });
+  const { DefaultAzureCredential } = require('@azure/identity');
+  const { SecretClient } = require('@azure/keyvault-secrets');
+  const credential = new DefaultAzureCredential();
+  const vaultName = 'sendemail1KV';
+  const url = `https://${vaultName}.vault.azure.net`;
+  const client = new SecretClient(url, credential);
 
-        const mailOptions = {
-            from: "kffsande@outlook.com",
-            to: "kffsande@outlook.com",
-            subject: req.body.emailSubject,
-            text: req.body.emailBody,
-            html: '<div><table><thead><tr><th>Product ID</th><th>Name</th></tr><thead><tbody>' + req.body.emailBody + '</tbody></table></div>',
-        }
+  const userRetrievedSecret = await client.getSecret('username1');
+  const username1 = userRetrievedSecret.value;
+  const pwdRetrievedSecret = await client.getSecret('password1');
+  const password1 = pwdRetrievedSecret.value;
 
-        transporter.sendMail(mailOptions,(error, info)=>{
-            if(error){
-                console.log(error);
-            }else{
-                console.log("Sent: " + info.response); 
-            }
-        }) 
+  let transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+      user: username1,
+      pass: password1,
+    },
+  });
+
+  const mailOptions = {
+    from: 'kffsande@outlook.com',
+    to: 'kffsande@outlook.com',
+    subject: req.body.emailSubject,
+    text: req.body.emailBody,
+    html:
+      '<div><table><thead><tr><th>Product ID</th><th>Name</th></tr><thead><tbody>' +
+      req.body.emailBody +
+      '</tbody></table></div>',
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Sent: ' + info.response);
+    }
+  });
 };
